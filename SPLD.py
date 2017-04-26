3057039796ec830276070b158b0e526518f61d53
Ginotuch <6973573+Ginotuch@users.noreply.github.com>
2017-04-27 01:31:00 +1200

    Improvements to edit list and error handling
    Changes:
    -Fixed some errors when trying to exit certain menus
    -Optimised some logic
    -Added a lot to the edit menu
    -Added better naming restrictions for lists
    -Re-ordered most lists to make while loops easier
    -Improved the commenting in some sections and added doc string

--- SPLD.py
+++ SPLD.py
@@ -1,4 +1,14 @@
+"""
+SPLD
+----
+
+This is a Shitty Python List Database that I am making just to learn and practice basic Python.
+Features include creating, editing, displaying, and saving user created lists.
+
+Author: Ginotuch
+"""
 from collections import defaultdict
+from re import match
 
 
 def menu_options():
@@ -11,20 +21,32 @@ def menu_options():
     print("  5. Delete list")
     print("  6. Edit list IN PROGRESS")
     print("  7. Import list UNFINISHED")
-    print("  8. Exit UNFINISHED (STILL WORKS)")
-    print("  0. Save database to file UNFINISHED")
+    print("  8. Save database to file UNFINISHED")
+    print("  9. Exit UNFINISHED (STILL WORKS)")
 
 
-def user_input(maximum):  # Get's user input for menu choice, repeats till acceptable range then returns choice.
+def user_input(maximum, exit_key, allowed_text):  # Choice selecting, also handles exiting/cancelling
     choice = None
     while choice is None:
+        input_string = "Select your choice: "
+        initial_input = input(input_string)
         try:
-            choice = int(input("Select your choice: "))
+            choice = int(initial_input)
             while choice > maximum or choice < 0:
-                print("Just read the damn options,", choice, "isn't an option")
-                choice = int(input("Select your choice: "))
+                if choice == exit_key:
+                    break
+                else:
+                    print("Just read the damn options,", choice, "isn't an option")
+                    choice = int(input("Select your choice: "))
         except ValueError:
-            print("\nEnter a number between 0 and", maximum, "(It's really not that hard)\n")
+            if (initial_input.isalpha()) and (initial_input.lower() == allowed_text.lower()) \
+                    and initial_input.lower() != "none":
+                if exit_key is None:  # This is here in case any number could be a valid option
+                    return False
+                else:
+                    return exit_key
+            else:
+                print(" -Invalid input")
     return choice
 
 
@@ -52,14 +74,16 @@ def store_in_list(list_name):
 
 
 def name_list():  # Names a newly created list (From option 3)
-    banned_names = ["exit", "cancel", "", " ", "ben"]
+    banned_names = ["exit", "cancel", ""]
     found = None
     name = None  # This isn't needed, it's just so that pycharm stops complaining
     while found is None:
         name = str(input("Enter a list name: "))
-        if name in banned_names:
+        if name.lower() in banned_names:
             print("Name not allowed")
             continue
+        if not match("^[a-zA-Z0-9_.-]*$", name):
+            print("Name must have characters, and no spaces or special characters.")
         else:
             found = True
     return name
@@ -81,17 +105,17 @@ def print_list_names(database):
         print(" -" + str(i))  # prints names of listed in format: "-list_name"
 
 
-def create_a_list(database):
+def create_a_list(database):  # Todo: Check if name already exists
     print("\nCreate a list:")
     print("-" * len("Create a list:"))
     name = name_list()  # Gets name for list
     temp = store_in_list(name)  # Gets data for list, if user cancels then it will return with None
-    if temp is None:
-        return
-    else:
+    if temp is not None:
         database[name] = temp
-        print(database)
+        print("Successfully created list")
         return database
+    else:
+        print("Cancelled")
 
 
 def check_list_exists(database):
@@ -116,11 +140,11 @@ def delete_list(database):
         print("  1. Print list names")
         print("  2. Print list contents by name")
         print("  3. Delete list by name")
-        print("  0. Exit")
+        print("  9. Exit")
     delete_menu()
     option = 1
     while option <= 3:
-        option = user_input(3)
+        option = user_input(3, 9, "exit")
         if option == 1:
             print_list_names(database)
         elif option == 2:
@@ -134,7 +158,8 @@ def delete_list(database):
                 return database
             else:
                 print("Cancelled")
-        elif option == 0:
+                return
+        elif option == 9:
             print("Cancelled")
             return
 
@@ -155,11 +180,12 @@ def edit_list(database):
             print("  1. Print list names")
             print("  2. Print list contents by name")
             print("  3. Edit list by name")
-            print("  0. Exit")
+            print("  4. Merge lists UNFINISHED")  # Todo
+            print("  9. Exit")
         edit_menu()
         option = 1
-        while option <= 3:
-            option = user_input(3)
+        while option <= 4:
+            option = user_input(4, 9, "exit")
             if option == 1:
                 print_list_names(database)
             elif option == 2:
@@ -170,35 +196,70 @@ def edit_list(database):
                 if checked_exists is not None:
                     print("\nContents of", checked_exists + ":")
                     print(", ".join(str(x) for x in database[checked_exists]))
-                    for x in range(0, len(database[checked_exists])):
-                        first_spaces = " " * (len(database[checked_exists][x]) // 2)
-                        second_spaces = " " * ((len(database[checked_exists][x]) - (len(database[checked_exists][x]) // 2)) + 1)
-                        print(first_spaces + str(x) + second_spaces, end="")
-                    print()
 
-                    def edit_menu():
+                    def edit_menu_2():
                         print("\nEditing:", checked_exists)
                         print("-" * len("Edit lists"))
-                        print("  1. Print ldasfist names")
-                        print("  2. Print lisasdft contents by name")
-                        print("  3. Edit list by name")
-                        print("  0. Exit")
-                    edit_menu()
-                    while option <= 3:
-                        option = user_input(3)
+                        print("  1. Delete an item")
+                        print("  2. Add an item")
+                        print("  3. Sort list (idk if I'll end up doing this)")
+                        print("  9. Exit")
+                    edit_menu_2()
+                    option_2 = 1  # This is just a choice tracker, it doesn't mean they selected menu option 2.
+                    while option_2 <= 3:
+                        option_2 = user_input(3, 9, "exit")
+                        if option_2 == 1:
+                            def nice_print(database_print, checked_exists_print):
+                                print(", ".join(str(x) for x in database_print[checked_exists_print]))
+                                # Puts index numbers under the center of each item in the selected list.
+                                for x in range(0, len(database_print[checked_exists_print])):
+                                    first_spaces = " " * (len(database_print[checked_exists_print][x]) // 2)
+                                    second_spaces = " " * ((len(database_print[checked_exists_print][x]) -
+                                                            (len(database_print[checked_exists_print][x]) // 2)) + 1)
+                                    print(first_spaces + str(x) + second_spaces, end="")
+                                print()
+                            print("\nChose from the index which item is deleted from:", checked_exists)
+                            option_3 = False
+                            while not option_3:
+                                nice_print(database, checked_exists)
+                                option_3 = user_input((len(checked_exists) - 1), None, "exit")
+                                if not option_3:
+                                    print("\nCancelled")
+                                    edit_menu_2()
+                                    break
+                                else:
+                                    del database[checked_exists][option_3]
+                                    print("\nSuccessfully deleted\nNew list:")
+                                    nice_print(database, checked_exists)
+                                    edit_menu_2()
+                        elif option_2 == 2:
+                            print("Add stuff")
+                        elif option_2 == 3:
+                            print("Print sort list")
+                        elif option_2 == 9:
+                            print("Cancelled")
+                            edit_menu()
                 else:
-                    print("Cancelled")
-            elif option == 0:
-                print("Cancelled")
+                    print(" Cancelled")
+                    edit_menu()
+
+            elif option == 4:
+                print("Todo merge")
+            elif option == 9:
+                print(" Cancelled")
+                edit_menu()
                 return
 
 
 def main():
     lists = defaultdict(list)
     menu_options()
-    option = user_input(8)
+    option = user_input(8, 9, "NONE")
+
+    # For testing, creates temp list
     test = 'name'
-    lists[test] = ['ab', 'asadfasdbc', 'abcd', 'abcsdffde', 'abcdef']
+    lists[test] = ['ab', 'testing', 'word', 'characters', 'And other things']
+
     # User choice selection till they decide to exit
     while option <= 8:
         if option == 1:  # print all names
@@ -212,29 +273,29 @@ def main():
             # Todo: Print all lists in columns, truncate if needed
         elif option == 4:  # Create a new list
             temp_lists = create_a_list(lists)
-            if temp_lists is None:
-                print("Cancelled")
-            else:
+            if temp_lists is not None:
                 lists = temp_lists
-        elif option == 5:
+        elif option == 5:  # Delete list
             if check_database_size(lists) is True:
-                lists = delete_list(lists)
-        elif option == 6:  # Edit a list TODO
+                temp_lists = delete_list(lists)
+                if temp_lists is not None:
+                    lists = temp_lists
+        elif option == 6:  # Edit a list TODO: In progress
             edit_list(lists)
         elif option == 7:
             print("todo 7")
             # Todo: Import list
-        elif option == 8:  # exit
-            # Todo: Check for unsaved changes
-            break
-        elif option == 0:  # save
+        elif option == 8:  # save
             if check_database_size(lists) is True:
                 print("Todo 0")
             # Todo: Save to file. Keeps history of last file, checks for overwrite.
-            print("todo 0")
+            print("todo 9")
+        elif option == 9:  # exit
+            # Todo: Check for unsaved changes
+            break
         print("\n")
         menu_options()
-        option = user_input(8)
+        option = user_input(8, 9, "NONE")
     print("\n\nk bye")
 
 
